740b02ac188be9bc2bebb9860e76220052136fb0
Luigi Paolo Favini <81562539+LuigiPaolo@users.noreply.github.com>
2026-06-29 00:25:21 +0200

    Update fader feedback plugin to v2.0 with fixes

--- faderFeedback.lua
+++ faderFeedback.lua
@@ -1,62 +1,67 @@
--- Plugin: Chataigne Fader Feedback (Solo OSC, Delta Tracking, Pagina Attiva)
--- Architettura ottimizzata per live: nessun traffico superfluo e zero conflitti MIDI.
+-- Plugin: Chataigne Fader Feedback v2.0 (Solo OSC, Delta Tracking, Ghost/Null Fix)
+-- Fix implementato: Riconoscimento "esecutori fantasma" su pagine vuote
 
-local osc_line = 1        -- INSERISCI QUI la riga del tuo setup OSC in MA3 (es. 1, 2, 3...)
-local update_rate = 0.05  -- 50 millisecondi (20Hz). Bilanciamento perfetto tra reattività e carico CPU
+local osc_line = 1        -- La tua riga OSC
+local update_rate = 0.05  -- 20Hz, bilanciamento ottimale
 
--- Tabelle per tracciare lo stato precedente. 
--- Inizializzate a -1 in modo che al primo avvio aggiornino subito i motori.
 local last_fader_values = {-1, -1, -1, -1, -1, -1, -1, -1}
 local last_master_value = -1
 
 local function FaderFeedbackLoop()
     while true do
         
-        -- 1. CONTROLLO DEGLI 8 FADER SULLA PAGINA ATTIVA
+        -- 1. CONTROLLO FADER
         for fader_index = 1, 8 do
             local exec_num = 200 + fader_index 
-            
-            -- GetExecutor(numero) restituisce l'esecutore sulla PAGINA ATTIVA. (Velocissimo)
             local my_exec = GetExecutor(exec_num)
             
+            local is_valid_fader = false
+            local current_val = 0
+            
+            -- Se l'esecutore esiste (anche come placeholder vuoto)
             if my_exec ~= nil then
-                -- Legge il valore attuale del fader (0 - 100)
-                local current_val = my_exec:GetFader({}) 
-                
-                -- DELTA TRACKING: Invia l'OSC SOLO se il fader è stato mosso o è cambiata la pagina
+                -- Usiamo pcall per interrogare l'API in modo blindato ed evitare crash
+                pcall(function()
+                    -- La proprietà .Object è nil se l'esecutore non ha assegnazioni (es. Sequence)
+                    if my_exec.Object ~= nil then
+                        local fader_check = my_exec:GetFader({})
+                        -- Se ha un fader leggibile, allora è valido
+                        if fader_check ~= nil then
+                            current_val = fader_check
+                            is_valid_fader = true
+                        end
+                    end
+                end)
+            end
+            
+            if is_valid_fader then
+                -- CASO A: L'esecutore è PIENO e VALIDO
                 if current_val ~= last_fader_values[fader_index] then
-                    
                     local osc_val = current_val / 100
-                    -- Invia a Chataigne (es: /xtouch/fader/1,f,0.850)
                     Cmd(string.format('SendOSC %d "/xtouch/fader/%d,f,%.3f"', osc_line, fader_index, osc_val))
-                    
-                    -- Aggiorna la memoria
                     last_fader_values[fader_index] = current_val
                 end
             else
-                -- GESTIONE DEL VUOTO: Se cambi su una pagina dove l'esecutore è vuoto (nil), 
-                -- il motore del fader deve andare a zero, ma solo una volta.
+                -- CASO B: L'esecutore è VUOTO o non assegnato (Cambio pagina su vuoto)
+                -- Se il fader fisico non è già a zero, lo abbassa immediatamente
                 if last_fader_values[fader_index] ~= 0 then
-                    Cmd(string.format('SendOSC %d "/xtouch/fader/%d,f,0.0"', osc_line, fader_index))
+                    Cmd(string.format('SendOSC %d "/xtouch/fader/%d,f,0.000"', osc_line, fader_index))
                     last_fader_values[fader_index] = 0
                 end
             end
         end
         
-        -- 2. CONTROLLO DEL GRAND MASTER (Fader 9 sull'X-Touch)
+        -- 2. CONTROLLO MASTER (Fader 9)
         local master_obj = Root().ShowData.Masters.Grand.Master
         if master_obj ~= nil then
             local current_master = master_obj.NormedValue
-            
             if current_master ~= last_master_value then
                 local osc_val = current_master / 100
-                -- Invia a Chataigne (es: /xtouch/master,f,0.500)
                 Cmd(string.format('SendOSC %d "/xtouch/master,f,%.3f"', osc_line, osc_val))
                 last_master_value = current_master
             end
         end
 
-        -- Rilascia il thread per non bloccare il sistema (essenziale per la stabilità in live)
         coroutine.yield(update_rate) 
     end
 end
